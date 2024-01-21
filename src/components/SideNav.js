4607e7878ddc21c46c3343c8c517170fb6c20c8e
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2024-01-21 21:46:48 +0530

    fix: Search button sidebar hidden for secured routes

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -116,6 +116,36 @@ export default function SideNav({ setKeyword, children }) {
     },
   ];
 
+  // disable search field for secured paths
+  const searchFieldContent =
+    pathName !== "/" && // skip pathName === '/' since it is also the homepage
+    secure.some((nav) => nav.href && nav.href === pathName) ? null : (
+      <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
+        <form className="flex flex-1" onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor="search-field" className="sr-only">
+            Search
+          </label>
+          <div className="relative w-full">
+            <MagnifyingGlassIcon
+              className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-500"
+              aria-hidden="true"
+            />
+            <input
+              id="search-field"
+              className="block h-full w-full border-0 bg-transparent py-0 pl-8 pr-0 text-white focus:ring-0 sm:text-sm"
+              placeholder="Search..."
+              type="search"
+              name="search"
+              onChange={(e) => {
+                e.preventDefault();
+                setKeyword(e.target.value);
+              }}
+            />
+          </div>
+        </form>
+      </div>
+    );
+
   return (
     <>
       <Transition.Root show={sidebarOpen} as={Fragment}>
@@ -392,31 +422,7 @@ export default function SideNav({ setKeyword, children }) {
             <span className="sr-only">Open sidebar</span>
             <Bars3Icon className="h-5 w-5" aria-hidden="true" />
           </button>
-
-          <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-            <form className="flex flex-1" onSubmit={(e) => e.preventDefault()}>
-              <label htmlFor="search-field" className="sr-only">
-                Search
-              </label>
-              <div className="relative w-full">
-                <MagnifyingGlassIcon
-                  className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-500"
-                  aria-hidden="true"
-                />
-                <input
-                  id="search-field"
-                  className="block h-full w-full border-0 bg-transparent py-0 pl-8 pr-0 text-white focus:ring-0 sm:text-sm"
-                  placeholder="Search..."
-                  type="search"
-                  name="search"
-                  onChange={(e) => {
-                    e.preventDefault();
-                    setKeyword(e.target.value);
-                  }}
-                />
-              </div>
-            </form>
-          </div>
+          {searchFieldContent}
         </div>
         {children}
       </div>
